3bd0904457ec9ee8aa39645c1c554a5c7202cde6
Chip Maurer <chip.maurer@dell.com>
2021-06-29 17:37:52 -0400

    Replace aws usage with s3curl
    Signed-off-by: Chip Maurer <chip.maurer@dell.com>

--- src/test/bin/s3_start.sh
+++ src/test/bin/s3_start.sh
@@ -17,33 +17,9 @@ export TXTFILE=$(readlink --canonicalize $SCRIPTDIR/../resources/datafile.txt)
 export PARQUET=$(readlink --canonicalize $SCRIPTDIR/../resources/customerfile)
 export PARQUET1=$(readlink --canonicalize $SCRIPTDIR/../resources/storefile)
 export CSVDIR=$(dirname $CSV)
-
-if [ ! -f /tmp/github.action.s3 ]; then
-    echo "Starting s3 docker container"
-    docker pull scality/s3server
-    docker run -d --name s3server -p $S3_DOCKER_PORT:$S3_DOCKER_PORT scality/s3server || exit 1
-else
-    rm -f /tmp/github.action.s3
-fi
-docker ps
-
-echo "TEST123A"
-
-aws help 2>/dev/null
-if [ $? -ne 0 ]; then
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-fi
-
-aws configure --profile s3connectortest << EOF > /dev/null
-$S3_ACCESS_KEY
-$S3_SECRET_KEY
-us-east-1
-json
-EOF
-
-echo "TEST123B"
+echo "Starting s3 docker container"
+docker pull scality/s3server
+docker run -d --name s3server -p $S3_DOCKER_PORT:$S3_DOCKER_PORT scality/s3server || exit 1
 
 found=0
 set -B                  # enable brace expansion
@@ -56,34 +32,63 @@ for i in {1..30}; do
     sleep 1
 done
 
-echo "TEST123C"
-
 if [ $found -eq 0 ]; then
     echo "Image run failed: docker run -d --name s3server -p $S3_DOCKER_PORT:$S3_DOCKER_PORT scality/s3server"
     exit 1
 fi
 
-echo "TEST123D"
+if [ ! -f /tmp/s3curl/s3curl.pl ]; then
+    pushd /tmp
+    git clone https://github.com/EMCECS/s3curl.git
+    chmod +x /tmp/s3curl/s3curl.pl
+    popd
+fi
+
+if [ -f ~/.s3curl ]; then
+    mv ~/.s3curl ~/.s3curl.bak.$$
+fi
+
+cat > ~/.s3curl << EOF
+%awsSecretAccessKeys = (
+    # personal account
+    # personal is a [friendly-name] . It can be named anything & is used in given s3curl commands.
+    scality => {
+        id => '$S3_ACCESS_KEY',
+        key => '$S3_SECRET_KEY',
+    },
+);
+
+push @endpoints , (
+    '127.0.0.1',
+);
+
+EOF
+
+chmod 600  ~/.s3curl
+
+# Sleep a bit for the s3 server to become ready
 
 echo "Creating bucket $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 mb s3://$S3_BUCKET/
+/tmp/s3curl/s3curl.pl --id=scality --createBucket -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET
 echo "Copy $CSV to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $CSV s3://$S3_BUCKET/
+/tmp/s3curl/s3curl.pl --id=scality --put=$CSV -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/`basename $CSV`
 echo "Copy $CSV1 to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $CSV1 s3://$S3_BUCKET/
+/tmp/s3curl/s3curl.pl --id=scality --put=$CSV1 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/`basename $CSV1`
 echo "Copy $CSV2 to $S3_BUCKET/grades"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $CSV2 s3://$S3_BUCKET/grades/grades.csv
+/tmp/s3curl/s3curl.pl --id=scality --put=$CSV2 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/grades/grades.csv
 echo "Copy $AVRODATA1 to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $AVRODATA1 s3://$S3_BUCKET/
+/tmp/s3curl/s3curl.pl --id=scality --put=$AVRODATA1 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/`basename $AVRODATA1`
 echo "Copy $JSON1 to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $JSON1 s3://$S3_BUCKET/cartoondb/cartoon_table.json
+/tmp/s3curl/s3curl.pl --id=scality --put=$JSON1 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/cartoondb/cartoon_table.json
 echo "Copy $JSON2 to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $JSON2 s3://$S3_BUCKET/jsondata/json_datafile
+/tmp/s3curl/s3curl.pl --id=scality --put=$JSON2 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/jsondata/json_datafile
 echo "Copy $PARQUET to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $PARQUET s3://$S3_BUCKET/customer/customerfile
+/tmp/s3curl/s3curl.pl --id=scality --put=$PARQUET -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/customer/customerfile
 echo "Copy $PARQUET1 to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $PARQUET1 s3://$S3_BUCKET/store/storefile
+/tmp/s3curl/s3curl.pl --id=scality --put=$PARQUET1 -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/store/storefile
 echo "Copy $TXTFILE to $S3_BUCKET"
-aws --profile s3connectortest --endpoint-url http://localhost:$S3_DOCKER_PORT s3 cp $TXTFILE s3://$S3_BUCKET/
+/tmp/s3curl/s3curl.pl --id=scality --put=$TXTFILE -- http://127.0.0.1:$S3_DOCKER_PORT/$S3_BUCKET/`basename $TXTFILE`
 
-netstat -tunlp
+if [ -f ~/.s3curl.bak.$$ ]; then
+    mv ~/.s3curl.bak.$$ ~/.s3curl
+fi
