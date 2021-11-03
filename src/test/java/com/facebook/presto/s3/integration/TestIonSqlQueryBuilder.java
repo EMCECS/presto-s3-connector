d13e1158a0991c8e6f227b52ff8c37588c703779
Andrew Robertson <2850052+adrdc@users.noreply.github.com>
2021-11-03 16:02:05 -0400

    Issue 67: add git commit hash to headers in class files used from prestodb (#71)
    * add pointer to code re-use in prestodb, add git commit hash
    
    Signed-off-by: Andrew Robertson <andrew.robertson@dell.com>
    
    * remove references to prestodb code in what i assume was due to previous copy/paste error
    
    Signed-off-by: Andrew Robertson <andrew.robertson@dell.com>

--- src/test/java/com/facebook/presto/s3/integration/TestIonSqlQueryBuilder.java
+++ src/test/java/com/facebook/presto/s3/integration/TestIonSqlQueryBuilder.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file is from prestodb Hive Connector
+ * https://github.com/prestodb/presto/blob/ca09cd31fedba77611975d5717cbaae80d2e984b/presto-hive/src/test/java/com/facebook/presto/hive/TestIonSqlQueryBuilder.java
  */
 
 package com.facebook.presto.s3.integration;
